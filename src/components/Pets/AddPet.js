3dd53e04b3c956e52346d91c59cfb1ed9a850b12
josepo <jlpolo@gmail.com>
2021-07-11 12:54:05 +0200

    Basic form validation

--- src/components/Pets/AddPet.js
+++ src/components/Pets/AddPet.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
@@ -5,19 +7,31 @@ import css from './AddPet.module.css';
 
 const AddPet = () =>
 {
+   const [name, setName] = useState('');
+   const [age, setAge] = useState('');
+
    const add = (event) =>
    {
       event.preventDefault();
+
+      if ((name.trim().length === 0) || (age.trim().length === 0))
+         return;
+
+      if (+age < 1)
+         return;
+
+      setName('');
+      setAge('');
    }
 
    return (
       <Card className={ css.input }>
          <form onSubmit={ add }>
             <label htmlFor='name'>Name</label>
-            <input id='name' />
+            <input id='name' value={ name } onChange={ e => { setName(e.target.value); }} />
 
             <label htmlFor='age'>Age</label>
-            <input id='age' type='number' />
+            <input id='age' value={ age } onChange={ e => { setAge(e.target.value); }} type='number' />
 
             <Button type='submit'>Add pet</Button>
          </form>
