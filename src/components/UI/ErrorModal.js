de36172e46e00c6fb317679bdc54c4dbdddd5a4b
josepo <jlpolo@gmail.com>
2021-07-16 17:12:08 +0200

    Render error modal in other portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,44 @@
+import ReactDOM from 'react-dom';
+
 import Card from './Card';
 import Button from './Button';
 
 import css from './ErrorModal.module.css';
 
+const Backdrop = ({ onClose }) =>
+   <div className={ css.backdrop } onClick={ onClose }/>;
+
+const Overlay = ({ msg, onClose }) =>
+{
+   return (
+      <Card className={ css.modal }>
+         <header className={ css.header }>
+            <h2>Error</h2>
+         </header>
+         <div className={ css.content }>
+            <p>{ msg }</p>
+         </div>
+         <footer className={ css.actions }>
+            <Button onClick={ onClose }>Ok</Button>
+         </footer>
+      </Card>
+   );
+}
+
 const ErrorModal = ({ msg, onClose }) =>
 {
    return (
       <>
-         <div className={ css.backdrop } onClick={ onClose }/>
-         <Card className={ css.modal }>
-            <header className={ css.header }>
-               <h2>Error</h2>
-            </header>
-            <div className={ css.content }>
-               <p>{ msg }</p>
-            </div>
-            <footer className={ css.actions }>
-               <Button onClick={ onClose }>Ok</Button>
-            </footer>
-         </Card>
+         {
+            ReactDOM.createPortal(
+               <Backdrop onClose={ onClose } />,
+               document.getElementById('backdrop-root')) 
+         }
+         { 
+            ReactDOM.createPortal(
+               <Overlay msg={ msg } onClose={ onClose } />,
+               document.getElementById('overlay-root'))
+         }
       </>
    );
 }
